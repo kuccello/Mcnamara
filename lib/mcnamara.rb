976b272d0dcc46244f859e60740256692d0763b5
Kristan Krispy Uccello <kuccello@gmail.com>
2009-11-25 16:14:29 -0500

    -- fix to middleware

--- lib/mcnamara.rb
+++ lib/mcnamara.rb
@@ -65,7 +65,7 @@ module SoldierOfCode
       possible_overrides << "-#{spy_vs_spy.browser}-#{spy_vs_spy.version.major}.css" if spy_vs_spy.version.major
 
       possible_overrides << "-#{spy_vs_spy.browser}.css"
-      
+
       return possible_overrides
 
     end
@@ -73,45 +73,46 @@ module SoldierOfCode
     def call(env)
 
       begin
-      # establish what browser we are dealing with
-      # hey! is spy-vs-spy upstream?
-      spy_vs_spy = env['soldierofcode.spy-vs-spy']
-      unless spy_vs_spy
-        http_user_agent = env['HTTP_USER_AGENT']
-        env['soldierofcode.spy-vs-spy'] = SpyVsSpy.new(http_user_agent)
+        # establish what browser we are dealing with
+        # hey! is spy-vs-spy upstream?
         spy_vs_spy = env['soldierofcode.spy-vs-spy']
-      end
+        unless spy_vs_spy
+          http_user_agent = env['HTTP_USER_AGENT']
+          env['soldierofcode.spy-vs-spy'] = SpyVsSpy.new(http_user_agent)
+          spy_vs_spy = env['soldierofcode.spy-vs-spy']
+        end
 
-      # look for what css we are after
-      request_path = env['REQUEST_PATH']
-      if request_path =~ /\.css$/
+        # look for what css we are after
+        request_path = env['REQUEST_PATH']
+        if request_path =~ /\.css$/
 
-        # need a base directory to scann...
-        # TODO -- ripe for a cache
-        possible_overrides = build_possible_overrides(spy_vs_spy)
+          # need a base directory to scann...
+          # TODO -- ripe for a cache
+          possible_overrides = build_possible_overrides(spy_vs_spy)
 
-        Dir["#{@css_dir}/**/*.css"].each do |css_file|
-          possible_overrides.each do |override|
-            request_path_sub = request_path[1..-1].downcase.sub(/\.css/, override)
+          Dir["#{@css_dir}/**/*.css"].each do |css_file|
+            possible_overrides.each do |override|
+              request_path_sub = request_path[1..-1].downcase.sub(/\.css/, override)
 
-            file_of_intrest = request_path_sub.split("/").last
+              file_of_intrest = request_path_sub.split("/").last
 #            puts "#{__FILE__}:#{__LINE__} #{__method__} #{file_of_intrest.downcase} |||| #{css_file.downcase[1..-1]}"
 
-            if css_file[1..-1].downcase =~ Regexp.new("#{file_of_intrest.downcase}$")
+              if css_file[1..-1].downcase =~ Regexp.new("#{file_of_intrest.downcase}$")
 #              puts "#{__FILE__}:#{__LINE__} #{__method__} I'm IN!!"
-              # this is the file we want to serve
-              css = 'Oh oo - I droped the bomb'
-              File.open(css_file, "r") {|f| css = f.read}
-              return [200,{"Content-Type"=>"text/css"},css]
+                # this is the file we want to serve
+                css = 'Oh oo - I droped the bomb'
+                File.open(css_file, "r") {|f| css = f.read}
+                return [200, {"Content-Type"=>"text/css"}, css]
+              end
             end
           end
+
         end
-      else
-        @app.call(env)
-      end
       rescue => e
         puts "#{__FILE__}:#{__LINE__} #{__method__} #{e} - #{e.backtrace}"
       end
+
+      @app.call      
 
     end
   end
